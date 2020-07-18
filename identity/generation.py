5347534b64ff3e97f8db3dca0c6d109037bf51e8
Luka Kilic <kilic.luka@gmail.com>
2020-07-18 11:57:35 +0200

    First set of tests passing

--- identity/generation.py
+++ identity/generation.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import random
+import string
+
+
+CURRENCY_TRADES = []
+
+
 def generate():
-    return None
+    currency_trade_id = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+
+    while currency_trade_id in CURRENCY_TRADES:
+        currency_trade_id = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+
+    CURRENCY_TRADES.append(currency_trade_id)
+    return currency_trade_id
