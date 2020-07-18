352d305b510b7423b6bc4860c433dc14959928de
Luka Kilic <kilic.luka@gmail.com>
2020-07-18 12:54:18 +0200

    Bulk generation tests passing

--- identity/generation.py
+++ identity/generation.py
@@ -4,14 +4,22 @@ import random
 import string
 
 
-CURRENCY_TRADES = []
+CURRENCY_TRADE_IDS = {}
 
 
 def generate():
-    currency_trade_id = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+    currency_trade_id = get_seven_character_alphanum()
 
-    while currency_trade_id in CURRENCY_TRADES:
-        currency_trade_id = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+    while CURRENCY_TRADE_IDS.get(currency_trade_id):
+        currency_trade_id = get_seven_character_alphanum()
 
-    CURRENCY_TRADES.append(currency_trade_id)
+    CURRENCY_TRADE_IDS[currency_trade_id] = True
     return currency_trade_id
+
+
+def generate_bulk(number_of_ids_to_generate):
+    return [generate() for _ in range(number_of_ids_to_generate)]
+
+
+def get_seven_character_alphanum():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=7))
